3228b3b417156bf15de827e8e583d3b13b429e4a
Sphinx <104243528+LOLsphinx@users.noreply.github.com>
2022-04-24 19:02:47 +0800

    delete.sh
    delete.sh

--- delete.sh
+++ delete.sh
@@ -28,27 +28,6 @@ clear
 else [ "$del" = "no" ];
 echo "[We will not delete your software LOLsphinx..]"
 
-fi
-
-if [ "$del" = "Yes" ];
-then
-echo "[We're deleting your software LOLsphinx...]"
-
-sleep 2
-
-rm -rf LOLsphinx
-echo "[LOLsphinx is deleted..]"
-
-sleep 2
-
-echo "We're heading back to your Main Directory.."
-cd ~
-cd
-
-else [ "$del" = "No" ];
-then
-echo "[We will not delete your software LOLsphinx..]"
 
 fi
-
 clear
