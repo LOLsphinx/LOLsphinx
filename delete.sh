8ceb176e8253a7de1613a7deed4270da3ed71b4a
Sphinx <104243528+LOLsphinx@users.noreply.github.com>
2022-04-24 18:05:48 +0800

    delete.sh
    bash delete.sh

---
+++ delete.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+chmod +x delete.sh
+
+clear
+echo -n "Do u wnat to remove this Software? [yes] or [no]: "
+read del
+
+sleep 3
+
+if [ "$del" = "yes" ];
+then
+echo "[We're deleting your software LOLsphinx...]"
+
+sleep 2
+
+rm -rf LOLsphinx
+echo "[LOLsphinx is deleted..]"
+
+sleep 2
+
+echo "We're heading back to your Main Directory.."
+cd ~
+cd
+
+elif [ "$del" = "Yes" ];
+then
+echo "[We're deleting your software LOLsphinx...]"
+
+sleep 2
+
+rm -rf LOLsphinx
+echo "[LOLsphinx is deleted..]"
+
+sleep 2
+
+echo "We're heading back to your Main Directory.."
+cd ~
+cd
+
+
+
+clear
+
+else [ "$del" = "no" ];
+echo "[We will not delete your software LOLsphinx..]"
+
+elif [ "$del" = "No ];
+then
+echo "[We will not delete your software LOLsphinx..]"
+
+fi
